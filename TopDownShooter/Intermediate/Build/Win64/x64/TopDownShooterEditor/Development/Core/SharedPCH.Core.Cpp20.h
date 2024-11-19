d880e7f659f8bb601978711328801cb12b8a056e
45P3R4 <langleybaka@gmail.com>
2024-11-19 19:07:40 +0300

    Initial commit

---
+++ TopDownShooter/Intermediate/Build/Win64/x64/TopDownShooterEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "X:/UEPROJ/UETopDownShooter/TopDownShooter/Intermediate/Build/Win64/x64/TopDownShooterEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ TopDownShooter/Intermediate/Build/Win64/x64/TopDownShooterEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "X:/UEPROJ/UETopDownShooter/TopDownShooter/Intermediate/Build/Win64/x64/TopDownShooterEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
